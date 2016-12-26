8aad20e1ea9b9cff0f7332dae180f3bec4b18784
Josh Creamer <joshcreamer@joshs-macbook-pro-2.local>
2016-12-26 13:40:47 -0600

    Added record and stop recording buttons

--- pitch_perfect/ViewController.swift
+++ pitch_perfect/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var recordingLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAudio(_ sender: UIButton) {
+        print("hello world")
+        recordingLabel.text = "Recording in Progress"
+    }
 
+    @IBAction func stopRecording(_ sender: UIButton) {
+        print("stop button pressed")
+    }
 }
 
